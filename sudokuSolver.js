7759ef15b627aad4d1adef7abf1794b6ca59877c
retsek860 <kestergriffiths@gmail.com>
2020-06-11 21:53:59 +0100

    Sudoku solver is fully functional!

--- sudokuSolver.js
+++ sudokuSolver.js
@@ -3,6 +3,70 @@ function solveSudoku() {
     givensGrid = createGivensGrid();
     workingGrid = JSON.parse(JSON.stringify(givensGrid))
 
+    var row = 0;
+    var column = 0;
+    var goingForward = true;
+    var working = true;
+
+    while (working) {
+
+        console.log(row, column, goingForward)
+
+        if ((row < 0) || (column < 0)) {
+            alert("Cannot solve this sudoku!");
+            working = false;
+
+        }
+        else if (row > 8) {
+            fillInSudoku(workingGrid);
+            alert("Solved!")
+            working = false;
+        }
+
+        else if (isGiven(givensGrid, row, column)) {
+            temp = updateCoords(row, column, goingForward);
+            row = temp[0];
+            column = temp[1];   
+        }
+        
+        else if (workingGrid[row][column] == 9) {
+            workingGrid[row][column] = 0;
+            goingForward = false; // Don't think this line is necessary, once code is complete consider taking this line out.
+            temp = updateCoords(row, column, goingForward);
+            row = temp[0];
+            column = temp[1];
+        }
+
+        else {
+
+            var cont = true;
+            var value = workingGrid[row][column] + 1;
+
+            while ((cont == true) && (value < 10)) {
+
+                if (isValid(workingGrid, row, column, value)) {
+
+                    workingGrid[row][column] = value;
+
+                    goingForward = true;
+                    cont = false;
+
+                    value -= 1;
+                }
+
+                value += 1;
+            }
+            if (value == 10) {
+
+                workingGrid[row][column] = 0;
+                goingForward = false;
+            }
+
+            temp = updateCoords(row, column, goingForward);
+            row = temp[0];
+            column = temp[1];
+        }
+    }
 };
 
 function createGivensGrid() {
@@ -56,17 +120,43 @@ function isValid(grid, row, column, value) {
     }
 
     // Check in 3x3 box
-    var x = Math.floor(column/3);
-    var y = Math.floor(row/3);
+    var xminor = Math.floor(column/3);
+    var yminor = Math.floor(row/3);
     for (var i = 0; i < 3; i++) {
         for (var j = 0; j < 3; j++) {
-            if ((i == row) && (j == column)) { continue; }
-            if (grid[x + i][y + j] == value) {
-                return false;
+            if (((3*xminor + i) != column) && ((3*yminor + j) != row)) {
+                if (grid[3*yminor + j][3*xminor + i] == value) {
+                    return false;
+                }
             }
         }
     }
 
     return true;
+}
 
+function fillInSudoku(workingGrid) {
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            $('[data-row="' + i + '"][data-column="' + j + '"]').css("fontSize", 70);
+            $('[data-row="' + i + '"][data-column="' + j + '"]').html(workingGrid[i][j]);
+        }
+    }
+};
+
+function updateCoords(row, column, goingForward) {
+
+    if ((column == 8) && goingForward) {
+        return [row += 1, 0];
+
+    } else if ((column == 0) && (goingForward == false)) {
+        return [row -= 1, 8];
+
+    } else {
+        if (goingForward) {
+            return [row, column += 1];
+        } else {
+            return [row, column -= 1];
+        }
+    }
 }
